0f273a2e1466828dc13e4c9e6934b61b646afe6e
이진호 <skan1603@naver.com>
2024-06-02 17:43:10 +0900

    새로운 빌드 시스템 적용 . VS2022 사용

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char** argv)
 {
-    std::cout << "hello, opengl" << std::endl;
+    
+    SPDLOG_INFO("Hello, SPDLOG");
+    SPDLOG_INFO("Initialize glfw");
+    if (!glfwInit()) {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+        
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
+        nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    SPDLOG_INFO("End glfw window");
+    
+
+    std::cout << " Hello, World" << std::endl;
     return 0;
 }
